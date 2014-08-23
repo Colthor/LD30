20525a949f0bcc52b39baaa6e9f5dd68de98ebe5
Colthor <github@colthor.co.uk>
2014-08-23 15:44:55 +0100

    Let's try that again.

---
+++ Assets/CameraSizer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraSizer : MonoBehaviour {
+
+	private int PrevWidth;
+	private int PrevHeight;
+	private float PrevPPU;
+	public float PixelsPerUnit = 100f;
+
+	// Use this for initialization
+	void Start () {
+		ResizeCamera();
+	}
+
+	void ResizeCamera()
+	{
+		PrevWidth = Screen.width;
+		PrevHeight = Screen.height;
+		PrevPPU = PixelsPerUnit;
+		camera.orthographicSize = PrevHeight / (2.0f * PixelsPerUnit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(!(PrevWidth == Screen.width && PrevHeight == Screen.height && PixelsPerUnit == PrevPPU))
+		{
+			ResizeCamera();
+		}
+	}
+}
---
+++ Assets/ShipChunkScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipChunkScript : MonoBehaviour {
+
+	
+	public string[] ChunkLayouts =
+	{
+		  "XXX0X"
+		+ "X000X"
+		+ "X000X"
+		+ "X000X"
+		+ "XXXXX",
+
+		  "XXXXX"
+		+ "00000"
+		+ "XXXXX"
+		+ "XXXXX"
+		+ "XXXXX",
+		
+		  "XXXXX"
+		+ "00000"
+		+ "XXXXX"
+		+ "00000"
+		+ "XXXXX",
+		
+		  "XXXXX"
+		+ "0000X"
+		+ "XXX0X"
+		+ "XXX0X"
+		+ "XXX0X",
+		
+		  "X0XXX"
+		+ "00XXX"
+		+ "XXXXX"
+		+ "XXXXX"
+		+ "XXXXX",
+		
+		  "XXX0X"
+		+ "0000X"
+		+ "XXXXX"
+		+ "XXXXX"
+		+ "XXXXX",
+		
+		  "X0X0X"
+		+ "00X0X"
+		+ "XXX0X"
+		+ "0000X"
+		+ "XXXXX",
+		
+		  "X0XXX"
+		+ "00XXX"
+		+ "XXXXX"
+		+ "XXX00"
+		+ "XXX0X"
+	};
+
+	// Use this for initialization
+	void Start () {
+
+		TileMapScript tms = GetComponent<TileMapScript>();
+		tms.LevelData = ChunkLayouts[0];
+		tms.GenerateMap();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
